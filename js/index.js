44cc6ede4d47f2d09f7d08445bb3e731e7b4beb6
Justin Chang <justin1992611@gmail.com>
2020-01-14 21:18:02 -0800

    update

--- js/index.js
+++ js/index.js
@@ -7,14 +7,17 @@ animationTop.addEventListener("mouseenter", (e) => {
     e.target.style.transition = "transform 0.9s";
 });
 animationTop.addEventListener("mouseleave", (e) => {
+    e.stopPropagation();
     e.target.style.transform = "scale(1)";
 });
 
 
-//keydown
+//keydown keyup
 const body = document.querySelector('body');
 const header = document.querySelector('header');
 body.addEventListener("keydown", () => header.style.opacity = "0");
+body.addEventListener("keyup", () => header.style.opacity = "1");
+
 
 //wheel
 body.addEventListener('wheel', function alertMeOnce(){
@@ -22,9 +25,36 @@ body.addEventListener('wheel', function alertMeOnce(){
     body.removeEventListener('wheel', alertMeOnce);
   });
 
+//click
+const header2 = document.querySelectorAll("h2")
+header2.forEach( cl => {
+    cl.addEventListener("click", () => {
+        cl.style.color = "purple";
+    })
+})
 
 //double-click
-const nav = document.querySelector(".nav");
-nav.addEventListener("dblclick", (e) => {
-    e.target.style.backgroundColor = 'navy';
+const b = document.querySelectorAll("a");
+ b.forEach( db => {
+    db.addEventListener("dblclick", () => {
+   db.style.backgroundColor = 'navy';
   });
+});
+
+
+//drag
+const letsgopic = document.querySelector(".img-content > img" );
+letsgopic.addEventListener('drag', (e) => e.target.style.transform = 'scale(1.5)');
+
+
+//resize
+window.addEventListener('resize', () => alert("wait!"));
+
+//scroll
+window.addEventListener('scroll', () => alert("danger!"));
+
+//prevenDefault
+const stopLink = document.querySelector ("nav")
+stopLink.addEventListener("click", (e) => {
+    e.preventDefault();
+});
